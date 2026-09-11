bedfb50e0b16aae4df7c82673621d1c77bf89e44
YellowHub-Ukraine <dir.namicom@gmail.com>
2026-09-11 17:43:57 +0200

    style(admin): load match editor stylesheet

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import { store } from "./app/store";
 import { AdminFeatureSwitch } from "./AdminFeatureSwitch";
 import "./styles.css";
 import "./console.css";
+import "./admin-match.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
