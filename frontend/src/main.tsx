7d439fc0473f3d7c0b3ce235f263ea5fff951a6b
YellowHub-Ukraine <dir.namicom@gmail.com>
2026-09-11 17:43:27 +0200

    feat(admin): mount admin match feature routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import { store } from "./app/store";
-import { App } from "./App";
+import { AdminFeatureSwitch } from "./AdminFeatureSwitch";
 import "./styles.css";
 import "./console.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <HashRouter><App /></HashRouter>
+      <HashRouter><AdminFeatureSwitch /></HashRouter>
     </Provider>
   </StrictMode>,
 );
